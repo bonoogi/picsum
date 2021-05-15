4f1a406bf2d7c5942e04d49ac472510d37116d11
Koo Bonwook <bnoo1333@gmail.com>
2021-05-15 14:25:53 +0900

    hilt 설정 추가

--- app/src/main/java/com/bonoogi/picsum/PicsumApp.kt
+++ app/src/main/java/com/bonoogi/picsum/PicsumApp.kt
@@ -1,9 +1,11 @@
 package com.bonoogi.picsum
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * @author 구본욱(koo.bonwook@postype.com)
  */
+@HiltAndroidApp
 class PicsumApp: Application() {
 }
